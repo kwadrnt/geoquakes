4d18fb964f13de9958af72d9a71700c7b4dda3cd
kwadrnt <kevinctse@gmail.com>
2017-04-06 09:17:14 -0700

    got all markers showing on map

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,15 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 
 $(document).on("ready", function() {
 
+	initMap();
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 37.78, lng: -122.44},
+          zoom: 8
+        });
+       
+      }
+
   $.ajax ({
   	method: "GET",
   	url: weekly_quakes_endpoint,
@@ -13,37 +22,36 @@ $(document).on("ready", function() {
   	success: onSuccess,
   	error: onError
   })
+
   function onSuccess(json) {
   	console.log(json);
   	console.log(json.features[0].properties.mag);
   
    var quakes = json.features;
    
+   // for of loop (ES6 practice)
    for (quake of quakes) {
 
 	// declare variable for each string
 	var mag = quake.properties.mag;
-	var place = quake.properties.place;
 	var lat = quake.geometry.coordinates[0];
 	var lng = quake.geometry.coordinates[1];
 
+	// markers for our location in SF and all other cities with earthquake data
+	// must zoom out to see all markers!
+	marker = new google.maps.Marker({
+    map: map,
+    position: {lat: lat, lng: lng}
+  	});
 
 	console.log(mag);
 
-   	$('#info').append("<p>"+ "M " + mag + " - " + place + "</p>");
-
-   	// function quakeInfo() {
-
-   	// }
+   	$('#info').append("<p>"+ quake.properties.title + "</p>");
+   	
    }
-  	
-  	
-
   }
-
-
-
-  function onError(xhr, status, errorThrown) {
+// function incase if there is an error console will show us
+function onError(xhr, status, errorThrown) {
 	alert("Sorry, there was a problem!");
 	console.log("Error: " + errorThrown);
 	console.log("Status: " + status);
